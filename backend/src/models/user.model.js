b5e35b14da22f6e7eda517a48dfcf67e858366c6
abhiramkarapuri <abhiramkarapuri@gmail.com>
2026-03-04 17:22:51 +0530

    signup controller added

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,25 @@
+import mongoose, { mongo } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  email:{
+    type:String,
+    required:true,
+    unique:true
+  },
+  fullName:{
+    type:String,
+    required:true,
+    unique:true
+  },
+  password:{
+    type:String,
+    required:true,
+    minlength:6
+  },
+  profilePic:{
+    type:String,
+    default:""
+  }
+},{timestamps:true})
+
+export const User = mongoose.model("User",userSchema) // create a user model with userSchema
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,10 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import path from 'path';
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
 
-import authRoutes from './routes/auth.route.js';
-import messageRoutes from './routes/message.route.js';
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
@@ -12,21 +13,22 @@ const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(express.json()); //req.body
 
-app.use("/api/auth",authRoutes);
-app.use("/api/messages",messageRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 //make ready for deployment
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname,'../frontend/dist')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    //catch all routes and send react app
-    app.use((_,res)=>{
-        res.sendFile(path.join(__dirname,"../frontend", "dist","index.html"));
-    });
+  //catch all routes and send react app
+  app.use((_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
-app.listen(PORT,()=>{
-    console.log('Server is running on port '+PORT);
-})
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
+  connectDB();
+});
